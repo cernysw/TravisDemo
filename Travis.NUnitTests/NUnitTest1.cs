e0f2019e07e59cc464548ff17e369e55c0c7f4b1
David Cerny <David.Cerny@solarwinds.com>
2018-06-20 12:03:29 +0200

    adding nunit tests

---
+++ Travis.NUnitTests/NUnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace Travis.NUnitTests
+{
+    [TestFixture]
+    public class NUnitTest1
+    {
+        
+        [Test]
+        public void TestMethod1()
+        {
+
+        }
+
+
+        [Test]
+        public void TestMethod2()
+        {
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void TestMethod3()
+        {
+            Assert.Fail();
+        }
+    }
+}

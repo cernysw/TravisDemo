961e59d601d20434ddb902dc1386fd6cc91a05aa
David Cerny <David.Cerny@solarwinds.com>
2018-06-21 11:18:42 +0200

    adding nunit tests to build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 C:\nuget\nuget.exe restore ./Travis.Tests/Travis.Tests.sln
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe /p:Configuration=Release ./Travis.Tests/Travis.Tests.sln
+.\Travis.Tests\packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe ./Travis.NUnitTests/bin/Release/Travis.NUnitTests.dll
